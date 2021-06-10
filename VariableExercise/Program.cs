c546f677de2e20e7d54b88255598a41f58bbef83
JakelynnWalker <jakelynnwalker15@icloud.com>
2021-06-10 10:31:09 -0500

    VariableCode

---
+++ VariableExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariableExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jakelynn";
+            int age = 18;
+            char z = 'c';
+            bool learningCode = true;
+            double pi = 3.14;
+            decimal b = 1609.344m;
+
+            Console.WriteLine($"Hi, My Name is {name} and i am {age} years old. 2 + 2 is {learningCode} and i am looking for someone with the letter {z} in their name. The circumference of a cirle is {pi} and 1 mile in meters is {b}");
+
+        }
+    }
+}
